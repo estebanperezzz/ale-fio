9fb9e66b249115aafb3fc0b9481bb5b2349441d7
Esteban Pérez <estebanperezz.8008@gmail.com>
2025-01-07 11:30:40 -0300

    Slow effect

--- src/components/MusicPreferenceModal.tsx
+++ src/components/MusicPreferenceModal.tsx
@@ -22,18 +22,19 @@ export default function MusicPreferenceModal() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
       className="fixed inset-0 flex items-center justify-center z-50 w-screen h-screen"
     >
       <div className="absolute inset-0 overflow-hidden">
         <Image
-          src="/_DSC5047.jpg"
+          src="/_DSC5024.jpg"
           alt="Background"
           fill
           style={{ objectFit: 'cover' }}
           quality={100}
           priority
         />
-        <div className="absolute inset-0 bg-gradient-to-l from-[#A1B290] to-[#4E6E5D] opacity-80"></div>
+        <div className="absolute inset-0 bg-gradient-to-l from-[#A1B290] to-[#4E6E5D] opacity-70"></div>
       </div>
       <motion.div
         initial={{ scale: 0.9, opacity: 0 }}
@@ -44,7 +45,7 @@ export default function MusicPreferenceModal() {
           <motion.div
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.2 }}
+            transition={{ delay: 0.2, duration: 1, ease: "easeOut" }}
           >
             <Image
               src="/logo.svg"
@@ -58,7 +59,7 @@ export default function MusicPreferenceModal() {
           <motion.h1
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.4 }}
+            transition={{ delay: 0.4, duration: 1, ease: "easeOut" }}
             className="text-4xl md:text-5xl font-bold mb-4 text-white"
           >
             Bienvenidos a la boda de Ale y Fio
@@ -67,7 +68,7 @@ export default function MusicPreferenceModal() {
           <motion.h2
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.6 }}
+            transition={{ delay: 0.6, duration: 1, ease: "easeOut" }}
             className="text-2xl md:text-3xl font-bold mb-8 text-white"
           >
             ¿Quieres iniciar con música o sin música?
@@ -76,21 +77,21 @@ export default function MusicPreferenceModal() {
           <motion.div
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.8 }}
+            transition={{ delay: 0.8, duration: 1, ease: "easeOut" }}
             className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4 w-full max-w-md"
           >
-          <AnimatedButton
-            onClick={() => handlePreference(true)}
-            className="w-full bg-[#4e6e5d] hover:bg-[#445F51] rounded-full shadow-xl text-xl h-full font-semibold"
-          >
-            Con música
-          </AnimatedButton>
-          <AnimatedButton
-            onClick={() => handlePreference(false)}
-            className="w-full bg-[#B76111] hover:bg-[#96410C] rounded-full shadow-xl text-xl h-full font-semibold"
-          >
-            Sin música
-          </AnimatedButton>
+            <AnimatedButton
+              onClick={() => handlePreference(true)}
+              className="w-full bg-[#4e6e5d] hover:bg-[#445F51] rounded-full shadow-xl text-xl py-3 font-semibold text-white"
+            >
+              Con música
+            </AnimatedButton>
+            <AnimatedButton
+              onClick={() => handlePreference(false)}
+              className="w-full bg-[#B76111] hover:bg-[#96410C] rounded-full shadow-xl text-xl py-3 font-semibold text-white"
+            >
+              Sin música
+            </AnimatedButton>
           </motion.div>
         </div>
       </motion.div>
